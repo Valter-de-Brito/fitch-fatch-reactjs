0a08dd95a2cb6e25b7c213c3378b2fe7cf9bd744
Valter-de-Brito <valter.dbrito@gmail.com>
2020-08-26 20:20:29 +0100

    Finalized the project

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -3,17 +3,24 @@ import React from "react";
 interface ISquare {
   id: number;
   content: string;
+  isSelected: boolean;
 }
 
 interface IProps {
   square: ISquare;
-  handleButtonClick: () => void;
+  handleButtonClick: (idSquare: number) => void;
 }
 
-const Square: React.FC<IProps> = ({square, handleButtonClick}: <IProps>) => {
-	return (
-		<button className="square" onClick={() => handleButtonClick()}>{square.content}</button>
-	);
+const Square: React.FC<IProps> = ({ square, handleButtonClick }: IProps) => {
+  return (
+    <button
+      disabled={square.isSelected}
+      className={square.content ? `square ${square.content}` : "square"}
+      onClick={() => handleButtonClick(square.id)}
+    >
+      {square.content}
+    </button>
+  );
 };
 
 export default Square;
